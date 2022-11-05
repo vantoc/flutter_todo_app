37c352ceda2b409ad77061a58af2e84bc38a3191
vantoc <mayt3156@gmail.com>
2022-11-05 22:16:49 +0700

    Creating custom AppBar

---
+++ lib/screens/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_flutter/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        child: Column(),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: tdBGColor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset('assets/images/hinh4.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
